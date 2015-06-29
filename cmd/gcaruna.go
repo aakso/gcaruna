c3ec8ada0b03aafc55b31e212f8edcc1bc42a6ec
Anton Aksola <aakso@iki.fi>
2015-06-29 17:19:38 +0000

    Relative start time flag and minor polishing

--- cmd/gcaruna.go
+++ cmd/gcaruna.go
@@ -25,17 +25,19 @@ const (
 	JsonOutput
 	InfluxDbOutput
 
-	EnvPrefix = "CARUNA_"
+	EnvPrefix = "GCARUNA_"
 )
 
 type Config struct {
-	TimeStart time.Time
-	TimeStop  time.Time
-	Mode      OperatingMode
-	Output    OutputMode
-	CarunaUrl string
-	Location  string
-	Debug     bool
+	TimeStart      time.Time
+	TimeStop       time.Time
+	Mode           OperatingMode
+	Output         OutputMode
+	CarunaUrl      string
+	CarunaUsername string
+	CarunaPassword string
+	Location       string
+	Debug          bool
 	// InfluxDB output specific
 	InfluxDB *output.InfluxDBConfig
 	// Internal config parsing stuff
@@ -106,16 +108,25 @@ func (cfg *Config) Parse(args []string) error {
 	}
 
 	// Parse timestamps
-	cfg.TimeStart, err = time.Parse(time.RFC3339, *cfg.argmap["tstart"].(*string))
-	if err != nil {
-		return fmt.Errorf("Cannot parse time_start: %s", err)
+	start := *cfg.argmap["tstart"].(*string)
+	if start != "" {
+		cfg.TimeStart, err = time.Parse(time.RFC3339, *cfg.argmap["start"].(*string))
+		if err != nil {
+			return fmt.Errorf("Cannot parse time_start: %s", err)
+		}
+	} else {
+		cfg.TimeStart = time.Now().Add(-1 * *cfg.argmap["rel_tstart"].(*time.Duration))
 	}
+
 	cfg.TimeStop, err = time.Parse(time.RFC3339, *cfg.argmap["tstop"].(*string))
 	if err != nil {
 		return fmt.Errorf("Cannot parse time_stop: %s", err)
 	}
 
 	cfg.Debug = *cfg.argmap["debug"].(*bool)
+	cfg.CarunaUrl = *cfg.argmap["caruna_url"].(*string)
+	cfg.CarunaUsername = *cfg.argmap["username"].(*string)
+	cfg.CarunaPassword = *cfg.argmap["password"].(*string)
 	cfg.CarunaUrl = *cfg.argmap["caruna_url"].(*string)
 	cfg.Location = *cfg.argmap["location"].(*string)
 	cfg.InfluxDB = &output.InfluxDBConfig{}
@@ -135,33 +146,32 @@ func fatal(errs ...error) {
 	for _, err := range errs {
 		fmt.Fprintln(os.Stderr, "ERROR:", err)
 	}
-	panic("fatal error")
-}
-
-func init() {
 }
 
 func NewConfig() *Config {
 	cfg := &Config{}
 
-	cfg.FlagSet = flag.NewFlagSet("caruna", flag.ExitOnError)
+	cfg.FlagSet = flag.NewFlagSet("gcaruna", flag.ExitOnError)
 	fs := cfg.FlagSet
 	cfg.argmap = make(map[string]interface{})
 
 	// Defaults
-	time_start := time.Now().Add(-48 * time.Hour).Format(time.RFC3339)
+	rel_start := 48 * time.Hour
 	time_stop := time.Now().Format(time.RFC3339)
 	const (
 		mode   = "location"
 		output = "text"
 	)
 
-	cfg.argmap["tstart"] = fs.String("time_start", time_start, "Start time in ISO8601 format")
-	cfg.argmap["tstop"] = fs.String("time_stop", time_stop, "Stop time in ISO8601 format")
+	cfg.argmap["tstart"] = fs.String("start", "", "Start time in ISO8601 format")
+	cfg.argmap["tstop"] = fs.String("stop", time_stop, "Stop time in ISO8601 format")
+	cfg.argmap["rel_tstart"] = fs.Duration("rstart", rel_start, "Start time relative to now")
 	cfg.argmap["mode"] = fs.String("mode", mode, "Mode of operation (series, location)")
 	cfg.argmap["location"] = fs.String("location", "", "Selected location for the series mode (address or location id)")
 	cfg.argmap["output"] = fs.String("output", output, "Output mode (text, json, influxdb)")
-	cfg.argmap["caruna_url"] = fs.String("caruna_url", caruna.CarunaAuthStart, "Caruna URL")
+	cfg.argmap["caruna_url"] = fs.String("url", caruna.CarunaAuthStart, "Caruna URL")
+	cfg.argmap["username"] = fs.String("username", "", "Caruna Username")
+	cfg.argmap["password"] = fs.String("password", "", "Caruna Password")
 	cfg.argmap["debug"] = fs.Bool("debug", false, "true/false")
 	cfg.argmap["influxdb_host"] = fs.String("influxdb_host", "", "InfluxDB host:port")
 	cfg.argmap["influxdb_username"] = fs.String("influxdb_username", "", "InfluxDB username")
@@ -171,15 +181,13 @@ func NewConfig() *Config {
 	return cfg
 }
 
-func main() {
-	username := os.Getenv("CARUNA_USERNAME")
-	password := os.Getenv("CARUNA_PASSWORD")
-
+func CliMain() {
 	config = NewConfig()
 
 	err := config.Parse(os.Args[1:])
 	if err != nil {
 		fatal(err)
+		return
 	}
 
 	clientOpts := &caruna.ClientOpts{}
@@ -187,9 +195,10 @@ func main() {
 		clientOpts.Logger = log.New(os.Stderr, "", log.LstdFlags)
 	}
 
-	client, err := caruna.NewCarunaClient(config.CarunaUrl, username, password, clientOpts)
+	client, err := caruna.NewCarunaClient(config.CarunaUrl, config.CarunaUsername, config.CarunaPassword, clientOpts)
 	if err != nil {
 		fatal(err)
+		return
 	}
 	defer client.Logout()
 
@@ -203,6 +212,7 @@ func main() {
 	}
 	if err != nil {
 		fatal(err)
+		return
 	}
 	switch config.Output {
 	case TextOutput:
@@ -215,6 +225,7 @@ func main() {
 			influxOutput, err := output.NewInfluxDBOutput(config.InfluxDB)
 			if err != nil {
 				fatal(err)
+				return
 			}
 			if config.Debug {
 				influxOutput.SetLogger(log.New(os.Stderr, "", log.LstdFlags))
@@ -222,7 +233,12 @@ func main() {
 			err = influxOutput.WriteData(vals)
 			if err != nil {
 				fatal(err)
+				return
 			}
 		}
 	}
+}
+
+func main() {
+	CliMain()
 }
